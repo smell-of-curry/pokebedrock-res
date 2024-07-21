ff3b45e99c94a86bb586139ea87d110344215bec
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-07-21 08:52:16 -0400

    Update utils.ts
    Ability to hide warn messages

--- generator/utils.ts
+++ generator/utils.ts
@@ -1,8 +1,14 @@
 /**
+ * If warn messages should be hidden while trining to diagnose an error
+ */
+let hideWarnMessages = true;
+
+/**
  * Console.warns a message in a yellow color
  * @param message
  */
 function warn(message: any) {
+  if (hideWarnMessages) return;
   console.warn("\x1b[33m" + message + "\x1b[0m");
 }
 
