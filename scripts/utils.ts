b17663118861b8ab2725680c89d63628facf3dba
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-10-25 14:02:45 -0400

    Remove File Logging for scripts

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -2,7 +2,6 @@ import * as fs from "fs";
 import * as path from "path";
 
 export class Logger {
-  private static logFilePath: string | null = null;
   private static readonly COLORS: Record<LogType, string> = {
     info: "\x1b[32m", // Green
     warning: "\x1b[33m", // Yellow
@@ -12,33 +11,6 @@ export class Logger {
     trace: "\x1b[36m", // Cyan
     fatal: "\x1b[41m", // Red background
   };
-
-  /**
-   * Sets the directory where the log file will be stored.
-   * The log file is named with a timestamp indicating when the process started.
-   * @param directory - The directory path for the log file.
-   */
-  static setLogDirectory(directory: string): void {
-    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-    if (!fs.existsSync(directory)) {
-      fs.mkdirSync(directory, {
-        recursive: true,
-      });
-    }
-    this.logFilePath = path.join(directory, `log-${timestamp}.log`);
-  }
-
-  /**
-   * Logs a message to the file if a log directory has been set.
-   * @param message - The message to log.
-   * @param type - The type of log message.
-   */
-  private static logToFile(message: string, type: LogType): void {
-    if (this.logFilePath) {
-      const formattedMessage = `[${type.toUpperCase()}] ${message}\n`;
-      fs.appendFileSync(this.logFilePath, formattedMessage);
-    }
-  }
 
   /**
    * Formats a message with the given ANSI color code.
@@ -59,7 +31,6 @@ export class Logger {
     const colorCode = this.COLORS[type];
     const formattedMessage = this.formatMessage(message, colorCode);
     console.log(formattedMessage);
-    this.logToFile(message, type);
   }
 
   static info(message: string): void {
